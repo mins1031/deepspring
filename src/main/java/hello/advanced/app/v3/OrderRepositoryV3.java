ef3c9b60fc4005c408c8dbb81911c352c7a10ad4
mins1031 <urisegea@naver.com>
2022-06-11 17:18:40 +0900

    동시성 문제 코드 확인

---
+++ src/main/java/hello/advanced/app/v3/OrderRepositoryV3.java
@@ -0,0 +1,40 @@
+package hello.advanced.app.v3;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV3 {
+
+    private final LogTrace trace;
+
+    public void save(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderRepositoryV2.save()");
+
+            // 저장 로직 - 저장은 1초정도하는데 만약 id 값으로 ex가 오면 예외가 발생하는 구조다
+            if (itemId.equals("ex")) {
+                throw new IllegalStateException("예외 발생!");
+            }
+            sleep(1000);
+
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
